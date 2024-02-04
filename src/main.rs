bcf96b6fd586e2b8673dca7f036da612fa3a7e04
Nicolas Bourras <dev@nicolasb.fr>
2024-02-04 14:31:14 +0100

    Basic axum installation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new();
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
