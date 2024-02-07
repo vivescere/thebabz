d749bbb17e8a32233a8d4ffd89f854c327841a01
Nicolas Bourras <dev@nicolasb.fr>
2024-02-07 17:29:44 +0100

    Static files + robots.txt

--- src/router.rs
+++ src/router.rs
@@ -1,6 +1,9 @@
 use axum::Router;
+use tower_http::services::ServeDir;
 
 /// Lists all of the routes contained in the application.
 pub fn all() -> Router {
-    Router::new()
+    let serve_static = ServeDir::new("static");
+
+    Router::new().fallback_service(serve_static)
 }
